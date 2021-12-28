a00acd42d89653e1c716c92bd5b0456bbcf591c5
Gleb Trubanov <crew.gleb@gmail.com>
2021-12-29 00:13:02 +0300

    make some constrains

--- HomeWork1.2/ViewController.swift
+++ HomeWork1.2/ViewController.swift
@@ -9,7 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var heightStack: NSLayoutConstraint!
+    @IBOutlet var redSignalView: UIView!
+    @IBOutlet var yellowSignalView: UIView!
+    @IBOutlet var greenSignalView: UIView!
+    
+    @IBOutlet var buttonStart: UIButton!
+    
+    /* как сделать круги??? если нет мыслей, то сделать их фикс и меньшего размера (YES!) */
+    
     override func viewDidLoad() {
+        let heightStack: CGFloat = self.view.frame.height
+        redSignalView.layer.cornerRadius = heightStack/17.5
+        yellowSignalView.layer.cornerRadius = heightStack/17.5
+        greenSignalView.layer.cornerRadius = heightStack/17.5
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
