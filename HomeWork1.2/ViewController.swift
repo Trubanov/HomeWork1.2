efa7d59f7b7192de99725381265a173b93b00977
Gleb Trubanov <crew.gleb@gmail.com>
2021-12-28 21:57:12 +0300

    Initial Commit

---
+++ HomeWork1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork1.2
+//
+//  Created by Надежда Говор on 28.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
