4cfa1c506526f81489ec7def6c12d009d956798d
Gleb Trubanov <crew.gleb@gmail.com>
2022-01-03 23:06:40 +0300

    add some changes in stack rectangles code

--- HomeWork1.2/ViewController.swift
+++ HomeWork1.2/ViewController.swift
@@ -10,20 +10,25 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet var heightStack: NSLayoutConstraint!
+
     @IBOutlet var redSignalView: UIView!
     @IBOutlet var yellowSignalView: UIView!
     @IBOutlet var greenSignalView: UIView!
     
     @IBOutlet var buttonStart: UIButton!
-    
-    /* как сделать круги??? если нет мыслей, то сделать их фикс и меньшего размера (YES!) */
+
     
     override func viewDidLoad() {
-        let heightStack: CGFloat = self.view.frame.height
-        redSignalView.layer.cornerRadius = heightStack/17.5
-        yellowSignalView.layer.cornerRadius = heightStack/17.5
-        greenSignalView.layer.cornerRadius = heightStack/17.5
+        redSignalView.layer.cornerRadius = 40
+        yellowSignalView.layer.cornerRadius = 40
+        greenSignalView.layer.cornerRadius = 40
+        
+        
+        // не меняется радиус кнопки :(
+        
+        buttonStart.layer.cornerRadius = 300
+        
+        
         
         super.viewDidLoad()
         // Do any additional setup after loading the view.
