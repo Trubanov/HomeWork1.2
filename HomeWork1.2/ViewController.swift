d4ad45e93ca8b731db7fd5662d858a12a06fe4b7
Gleb Trubanov <crew.gleb@gmail.com>
2022-01-10 22:42:17 +0300

    DONE! (add  some action for button)

--- HomeWork1.2/ViewController.swift
+++ HomeWork1.2/ViewController.swift
@@ -15,9 +15,21 @@ class ViewController: UIViewController {
     @IBOutlet var yellowSignalView: UIView!
     @IBOutlet var greenSignalView: UIView!
     
+    enum Signals {
+        case redSignalView
+        case yellowSignalView
+        case greenSignalView
+    }
+    
+    var currentLight = Signals.redSignalView
+    
     @IBOutlet var buttonStarter: UIButton!
     
     override func viewDidLoad() {
+        redSignalView.alpha = 0.4
+        yellowSignalView.alpha = 0.4
+        greenSignalView.alpha = 0.4
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         redSignalView.layer.cornerRadius = 40
@@ -26,7 +38,30 @@ class ViewController: UIViewController {
         
         buttonStarter.layer.cornerRadius = 27
     }
-
-
+    
+   
+    @IBAction func addLight(_ sender: Any) {
+        
+        buttonStarter.setTitle("Next", for: .normal)
+    
+        switch currentLight {
+        case .redSignalView:
+            yellowSignalView.alpha = 0.4
+            greenSignalView.alpha = 0.4
+            redSignalView.alpha = 1
+            currentLight = .yellowSignalView
+        case .yellowSignalView:
+            yellowSignalView.alpha = 1
+            greenSignalView.alpha = 0.4
+            redSignalView.alpha = 0.4
+            currentLight = .greenSignalView
+        case .greenSignalView:
+            yellowSignalView.alpha = 0.4
+            greenSignalView.alpha = 1
+            redSignalView.alpha = 0.4
+            currentLight = .redSignalView
+        }
+    }
+    
 }
 
